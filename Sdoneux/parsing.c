72a1f6d7f2e03cd0d0fc100f85cbd49c54ce4bd4
Lucas Ide <lide@fu-r9-p6.s19.be>
2022-06-08 20:12:29 +0200

    sdoneux ajouter

---
+++ Sdoneux/parsing.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdoneux <sdoneux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 18:51:43 by sdoneux           #+#    #+#             */
+/*   Updated: 2022/06/08 19:28:36 by sdoneux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test.h"
+
+int	verify_buitlins(char *line)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strncmp(line, "echo", 5) != 0)
+		i = 1;
+	if (ft_strncmp(line, "cd", 2) != 0)
+		i = 1;
+	if (ft_strncmp(line, "pwd", 3) != 0)
+		i = 1;
+	if (ft_strncmp(line, "export", 7) != 0)
+		i = 1;
+	if (ft_strncmp(line, "unset", 6) != 0)
+		i = 1;
+	if (ft_strncmp(line, "env", 3) != 0)
+		i = 1;
+	if (ft_strncmp(line, "exit", 4) != 0)
+		i = 1;
+	return (i);
+}
+
+char	*get_cmd(char *path, char *cmd)
+{
+	char	*tmp;
+	char	*command;
+	char	**cmd_paths;
+
+	cmd_paths = ft_split(path, ':');
+	while (*cmd_paths)
+	{
+		tmp = ft_strjoin(*cmd_paths, "/");
+		command = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (access(command, 0) == 0)
+			return (command);
+		free(command);
+		cmd_paths++;
+	}
+	return (NULL);
+}
