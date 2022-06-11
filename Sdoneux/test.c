87818d9f40ac2a05ffbe272f84c478f601132739
doneuxsimon <73170968+doneuxsimon@users.noreply.github.com>
2022-06-11 18:52:35 +0200

    Update test.c

--- Sdoneux/test.c
+++ Sdoneux/test.c
@@ -1,19 +1,24 @@
 #include "test.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	char	*path;
 	char	*tmp;
+	int		i;
 
+	if (argc != 1)
+		exit(EXIT_SUCCESS);
+	if (strncmp(argv[0], "./Minishell", 7) != 0)
+		exit(EXIT_SUCCESS);
 	path = getenv( "PATH" );
 	while (1)
 	{
-		line = readline("Minishell $> ");
+		line = readline("Minishell$ ");
 		add_history(line);
+		i = verify_builtins(line, envp);
 		tmp = get_cmd(path, line);
-		printf("%s\n", tmp);
+		//printf("%s\n", tmp);
 	}
 	return (0);
 }
-//echo
